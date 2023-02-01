9b56cf98de8be9e399de7ee4149d86a8dc5a9667
Bruno Rennan <bruno_rennan@hotmail.com>
2023-02-01 17:41:33 -0300

    criação das pastas

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebae"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBd5QIZ4YXyJCUYQUP7bzkTmrA3tFQYAGU",
+  authDomain: "miniblog-5897a.firebaseapp.com",
+  projectId: "miniblog-5897a",
+  storageBucket: "miniblog-5897a.appspot.com",
+  messagingSenderId: "904061195917",
+  appId: "1:904061195917:web:81d6c3358422d935acb4c4",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
---
+++ src/pages/CreatPost/CreatPost.js
@@ -0,0 +1,9 @@
+import styles from "./CreatPost.module.css"
+
+const CreatPost = () => {
+  return (
+    <div>CreatPost</div>
+  )
+}
+
+export default CreatPost
